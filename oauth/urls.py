bf3b5c1e2a4cef67ed7459dd1c9427cf23b21315
Youngho Cho <dominoyh5@gmail.com>
2025-01-08 16:10:14 +0900

    add: oauth index page

--- oauth/urls.py
+++ oauth/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.index, name='index'),
     path('login/', views.login_view, name='login'),
     path('oauth_callback/', views.oauth_callback, name='oauth_callback'),
-    path('logout/', views.logout_view, name='logout'),
 ]
--- oauth/views.py
+++ oauth/views.py
@@ -1,18 +1,25 @@
 import requests
-from django.shortcuts import redirect
-from django.contrib.auth import logout
+from django.shortcuts import render, redirect
 from django.http import JsonResponse
 
 CLIENT_ID = 'u-s4t2ud-c5829f79ff491d8cd54c3732c3414af2e0124487aefdafe69862641dbbdc6398'
 CLIENT_SECRET = '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'
 REDIRECT_URI = 'http://localhost:8000/oauth/oauth_callback'
 
+def index(request):
+    jwt_token = request.COOKIES.get('jwt')
+    user_name = request.COOKIES.get('user_name')
+    
+    context = {
+        'user_authenticated': jwt_token is not None,
+        'user_name': user_name if user_name else '',
+    }
+    
+    return render(request, 'oauth/index.html', context)
+
 def login_view(request):
     authorize_url = f"https://api.intra.42.fr/oauth/authorize?client_id={CLIENT_ID}&redirect_uri={REDIRECT_URI}&response_type=code"
     return redirect(authorize_url)
-
-def logout_view(request):
-    return redirect('https://signin.intra.42.fr/users/sign_out')
 
 def oauth_callback(request):
     code = request.GET.get('code')
@@ -31,19 +38,21 @@ def oauth_callback(request):
     response = requests.post(token_url, data=data)
     if response.status_code == 200:
         token_data = response.json()
-        print(token_data)
-        access_token = token_data['access_token']
-        
-        user_info_url = 'https://api.intra.42.fr/v2/me'
-        headers = {
-            'Authorization': f'Bearer {access_token}'
-        }
-        user_info_response = requests.get(user_info_url, headers=headers)
-        
-        if user_info_response.status_code == 200:
-            user_info = user_info_response.json()
-            return JsonResponse(user_info)
-        else:
-            return JsonResponse({'error': 'Failed to fetch user info'}, status=400)
+        return redirect('/oauth')
     else:
         return JsonResponse({'error': 'Failed to obtain token'}, status=400)
+
+def get_profile_picture(request):
+    access_token = token_data['access_token']
+    
+    user_info_url = 'https://api.intra.42.fr/v2/me'
+    headers = {
+        'Authorization': f'Bearer {access_token}'
+    }
+    user_info_response = requests.get(user_info_url, headers=headers)
+    
+    if user_info_response.status_code == 200:
+        user_info = user_info_response.json()
+        return JsonResponse(user_info)
+    else:
+        return JsonResponse({'error': 'Failed to fetch user info'}, status=400)
