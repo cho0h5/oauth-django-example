b032c415dc90a8dd4f8ded91077d97e74990253d
Youngho Cho <dominoyh5@gmail.com>
2025-01-08 16:57:19 +0900

    add: jwt

--- oauth/views.py
+++ oauth/views.py
@@ -1,10 +1,23 @@
 import requests
 from django.shortcuts import render, redirect
 from django.http import JsonResponse
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth.models import User
 
 CLIENT_ID = 'u-s4t2ud-c5829f79ff491d8cd54c3732c3414af2e0124487aefdafe69862641dbbdc6398'
 CLIENT_SECRET = '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'
 REDIRECT_URI = 'http://localhost:8000/oauth/oauth_callback'
+
+def get_or_create_user(user_info):
+    user, created = User.objects.get_or_create(
+        username=user_info['login'],
+        defaults={
+            'email': user_info['email'],
+            'first_name': user_info['first_name'],
+            'last_name': user_info['last_name'],
+        }
+    )
+    return user
 
 def index(request):
     jwt_token = request.COOKIES.get('jwt')
@@ -20,6 +33,12 @@ def index(request):
 def login_view(request):
     authorize_url = f"https://api.intra.42.fr/oauth/authorize?client_id={CLIENT_ID}&redirect_uri={REDIRECT_URI}&response_type=code"
     return redirect(authorize_url)
+
+def logout_view(request):
+    response = redirect('/oauth')
+    response.delete_cookie('jwt')
+    response.delete_cookie('user_name')
+    return response
 
 def oauth_callback(request):
     code = request.GET.get('code')
@@ -38,7 +57,28 @@ def oauth_callback(request):
     response = requests.post(token_url, data=data)
     if response.status_code == 200:
         token_data = response.json()
-        return redirect('/oauth')
+        access_token = token_data['access_token']
+        
+        user_info_url = 'https://api.intra.42.fr/v2/me'
+        headers = {'Authorization': f'Bearer {access_token}'}
+        user_info_response = requests.get(user_info_url, headers=headers)
+        
+        if user_info_response.status_code == 200:
+            user_info = user_info_response.json()
+            
+            user = get_or_create_user(user_info)
+            print(user)
+
+            refresh = RefreshToken.for_user(user)
+            jwt_access_token = str(refresh.access_token)
+
+            response = redirect('/oauth')
+            response.set_cookie('jwt', jwt_access_token, httponly=True, secure=True)
+            response.set_cookie('user_name', user_info['login'])
+            
+            return response
+        else:
+            return JsonResponse({'error': 'Failed to fetch user info'}, status=400)
     else:
         return JsonResponse({'error': 'Failed to obtain token'}, status=400)
 
--- oauth_django/settings.py
+++ oauth_django/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 import os
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -38,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework_simplejwt',
     'oauth'
 ]
 
@@ -123,3 +126,14 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+}
